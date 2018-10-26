0dab9632159add3a512348a7ba43eda2a635b0af
Rackeb Mered <rackeb.mered@duke.edu>
2018-10-25 23:13:54 -0400

    test

--- src/LinkStrand.java
+++ src/LinkStrand.java
@@ -96,7 +96,7 @@ public class LinkStrand implements IDnaStrand {
 			temp = temp.next;
 		}
 		LinkStrand finishing = new LinkStrand ();
-		
+		finishing.mySize= mySize; 
 		finishing.myFirst= current; 
 		finishing.myLast= current2; 
 		
